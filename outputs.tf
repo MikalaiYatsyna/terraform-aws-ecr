8d5683c5c12c0c149b901da56e85cd20f32f89e2
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2024-03-07 19:49:59 +0100

    Update descriptions (#3)

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "arn" {
-  value = module.ecr.repository_arn
+  value       = module.ecr.repository_arn
+  description = "Outputs the Amazon Resource Name (ARN) of the created ECR repository."
 }
 
 output "registry_id" {
-  value = module.ecr.repository_registry_id
+  value       = module.ecr.repository_registry_id
+  description = "Outputs the AWS account ID where the repository is located."
 }
 
 output "repository_url" {
-  value = module.ecr.repository_url
+  value       = module.ecr.repository_url
+  description = "Outputs the URL of the created ECR repository."
 }
